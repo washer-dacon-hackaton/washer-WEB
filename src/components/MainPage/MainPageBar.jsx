39927fa3c3cbc503521928a73f79c92ad9d3b820
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-07-17 04:02:45 +0900

    feat: MainPage 컴포넌트 생성

---
+++ src/components/MainPage/MainPageBar.jsx
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react";
+import { Button } from "wowds-ui";
+
+export const MainPageBar = () => {
+  return (
+    <div>
+      <div className={css``}>오늘 하루는 어떠셨나요?</div>
+      <Button></Button>
+    </div>
+  );
+};
