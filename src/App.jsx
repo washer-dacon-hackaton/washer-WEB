e880e09fa265f876d11822558b6a0d04fb3aec4c
Hyeok Lee <112482426+hhhkdev@users.noreply.github.com>
2024-07-30 04:37:17 +0900

    ⚙️ FEAT | 기본 컴포넌트 및 디자인시스템 구현 (#9)
    * chore: styled 설치
    
    * feat: 기본 컴포넌트 개발
    
    * doce: PR template 설정
    
    * chore: emotion-reset 설정
    
    * style: style 관련 파일들 세팅
    
    * chore: App.jsx 수정
    
    * feat: MyPage 컴포넌트 일부 생성
    
    * feat: MainPage 컴포넌트 생성
    
    * chore: 프로젝트 세팅
    
    * feat: Space 컴포넌트 최적화
    
    * feat: GlobalStyle 설정
    
    * chore: 폴더 구조 수정
    
    * feat: GNB 구현 중
    
    * feat: 디자인 시스템 등록
    
    * chore: yarn install
    
    * feat: GNB 구현
    
    * feat: Wrapper 구현 및 공부
    
    * feat: Button 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { GNB } from "./widgets/GNB";
 
 function App() {
-  return <></>;
-}
+  return (
+    <>
+      <GNB user={{ name: "이혁" }} breadcrumbs={"메인 페이지"}></GNB>
+    </>
+  );
+
 
 export default App;
