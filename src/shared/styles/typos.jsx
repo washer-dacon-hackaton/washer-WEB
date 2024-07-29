5a700c15770390fd3b880d06823f17cf16ed6b1b
Hyeok Lee <c235335@g.hongik.ac.kr>
2024-07-30 03:40:15 +0900

    feat: 디자인 시스템 등록

--- src/shared/styles/typos.jsx
+++ src/shared/styles/typos.jsx
@@ -4,19 +4,64 @@ import { css } from "@emotion/react";
 // calcRem이 필요한 이유? 단순 반응형에 대한 대응으로 필요한건가? 맞는 듯. 제외해도 됨.
 
 export const typo = {
-  heading: css`
-    font-family: "Pretendard";
-    /** 폰트 패밀리 자체에 대한 공부가 필요할 듯..*/
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 100%;
-    letter-spacing: 0%;
-  `,
-  default: css`
-    font-family: "Pretendard";
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 100%;
-    letter-spacing: 0%;
-  `,
+  Head: {
+    h0: css`
+      font-family: "Pretendard";
+      font-size: 64px;
+      font-weight: 900;
+      line-height: 150%;
+      letter-spacing: -2.5%;
+    `,
+    h1: css`
+      font-family: "Pretendard";
+      font-size: 24px;
+      font-weight: 700;
+      line-height: 150%;
+      letter-spacing: -2.5%;
+    `,
+    h2: css`
+      font-family: "Pretendard";
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 130%;
+      letter-spacing: -2.5%;
+    `,
+    h3: css`
+      font-family: "Pretendard";
+      font-size: 16px;
+      font-weight: 500;
+      line-height: 130%;
+      letter-spacing: -1%;
+    `,
+  },
+  Body: {
+    b0: css`
+      font-family: "Pretendard";
+      font-size: 20px;
+      font-weight: 400;
+      line-height: 130%;
+      letter-spacing: -2.5%;
+    `,
+    b1: css`
+      font-family: "Pretendard";
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 160%;
+      letter-spacing: -2.5%;
+    `,
+    b2: css`
+      font-family: "Pretendard";
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 130%;
+      letter-spacing: -2.5%;
+    `,
+    b3: css`
+      font-family: "Pretendard";
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 130%;
+      letter-spacing: 0%;
+    `,
+  },
 };
